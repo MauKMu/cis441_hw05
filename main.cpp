ab663e8679ff76878e7a739404bf446f30ce1cf8
MauKMu <mkenzom@gmail.com>
2016-10-21 01:24:16 -0400

    really finished main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include <time.h> 
 
 #define PADDLE_X_INC        2
 #define PADDLE_MIN          3
@@ -9,24 +10,45 @@
 #define BALL_X_MIN          1.0f
 #define BALL_X_MAX          60.0f
 #define BALL_Y_MIN          1.0f
-#define BALL_Y_MAX          30.0f
+#define BALL_Y_MAX          29.0f
 
-#define SPEED_INC           1.05f
+#define SPEED_INIT_MAG      0.25f
+#define SPEED_INC           1.2f
+#define RAND_INT_MAX        1000
 
+
+
+
+AnalogIn ain(p20); // should be floating
 InterruptIn left(p5);
 InterruptIn right(p6);
 Ticker ticker;
 
+uint16_t lfsr_value;
 int paddle_x = 30;
 
 float ball_x = 30.0f;
 float ball_y = 5.0f;
 
-float ball_speed_x = (float) rand() / RAND_MAX;
-float ball_speed_y = (float) rand() / RAND_MAX;
+float ball_speed_x;
+// find ball_speed_y to keep magnitude of speed as SPEED_INIT_MAG
+// 50/50 chance of being negative
+float ball_speed_y;
 
 int moving_left = false;
 int moving_right = false;
+
+
+void seed_lfsr() {
+    lfsr_value = ain.read_u16();
+}    
+
+uint16_t lfsr() {
+    uint16_t new_bit = (lfsr_value >> 15 & 0x0001) ^ (lfsr_value >> 13 & 0x0001) ^ 
+                        (lfsr_value >> 12 & 0x0001) ^ (lfsr_value >> 10 & 0x0001);
+    lfsr_value = (lfsr_value << 1) | new_bit;
+    return lfsr_value;
+}    
 
 void handle_button_change() {
     // update status of paddle
@@ -60,21 +82,21 @@ void update_board() {
     // vertical wall collisions
     if (ball_x > BALL_X_MAX) {
         ball_x = BALL_X_MAX;
-        ball_speed_y *= -1.0f;
+        ball_speed_x *= -1.0f;
     }
     else if (ball_x < BALL_X_MIN) {
         ball_x = BALL_X_MIN;    
-        ball_speed_y *= -1.0f;
+        ball_speed_x *= -1.0f;
     }
     
     // horizontal (top) wall collision
     if (ball_y < BALL_Y_MIN) {
         ball_y = BALL_Y_MIN; 
-        ball_speed_x *= -1.0f;  
+        ball_speed_y *= -1.0f;  
     }
     else if (ball_y > BALL_Y_MAX) {
         // check if paddle is there!    
-        if ((float)(paddle_x - 2) <= ball_x && ball_x <= (float)(paddle_x + 2)) {
+        if ((float)(paddle_x - 3) <= ball_x && ball_x <= (float)(paddle_x + 3)) {
             // paddle is under ball, so just reflect
             ball_y = BALL_Y_MAX;
             // increase speed (and reflect y)
@@ -93,10 +115,14 @@ void update_board() {
     printf("p%02d\n", paddle_x);
 
     // ball
-    printf("b%02d,%02d\n", (int)ball_x, (int)ball_y);
+    printf("b%02d,%02d\n", (int)rint(ball_x), (int)rint(ball_y));
 }
 
 int main() {
+    seed_lfsr();
+    ball_speed_x = (float) (lfsr()) * (lfsr() % 2 ? -1.0f : 1.0f) * SPEED_INIT_MAG / 65536.0f;
+    ball_speed_y = (lfsr() % 2 ? -1.0f : 1.0f) * sqrt(SPEED_INIT_MAG * SPEED_INIT_MAG - ball_speed_x * ball_speed_x);
+    
     left.rise(&handle_button_change);    
     left.fall(&handle_button_change);    
     right.rise(&handle_button_change);    
